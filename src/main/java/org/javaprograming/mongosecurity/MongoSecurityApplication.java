ee801c69c513dbf3342c31e83e4a62c1d0ea5bd1
ononoki0117 <tytttyty@naver.com>
2024-06-11 23:04:36 +0900

    API 구현 완료

--- src/main/java/org/javaprograming/mongosecurity/MongoSecurityApplication.java
+++ src/main/java/org/javaprograming/mongosecurity/MongoSecurityApplication.java
@@ -2,8 +2,11 @@ package org.javaprograming.mongosecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableMongoRepositories
 public class MongoSecurityApplication {
 
     public static void main(String[] args) {
